aa1e16a27ddddd59b3cdfb14c01f552c5515d141
Neda Pirayesh <nd_pirayesh@yahoo.com>
2020-08-04 16:44:47 +0430

    Add Employee Controller- Add ViewModel of Employee index

---
+++ PayCompute.Services/ViewModels/EmployeeIndexViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PayCompute.Services.ViewModels
+{
+   public class EmployeeIndexViewModel
+    {
+        public int Id { get; set; }
+        public string EmployeeNo { get; set; }
+        public string FullName { get; set; }
+        public string Gender { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime DateJoin { get; set; }
+    }
+}
---
+++ PayComputee/Controllers/EmployeeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PayCompute.Services;
+using PayCompute.Services.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PayComputee.Controllers
+{
+    public class EmployeeController: Controller
+    {
+        private readonly IEmployeeService _employeeService;
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        public IActionResult Index()
+        {
+            var employees = _employeeService.GetAll().Select(employee => new EmployeeIndexViewModel
+            {
+                Id=employee.Id,
+                EmployeeNo=employee.EmployeeNo,
+                ImageUrl=employee.ImageUrl,
+                FullName=employee.FullName,
+                Gender=employee.Gender,
+                City=employee.City,
+                Designation=employee.Designation,
+                DateJoin=employee.DateJoined
+            }).ToList();
+            return View(employees);
+        }
+    }
+}
