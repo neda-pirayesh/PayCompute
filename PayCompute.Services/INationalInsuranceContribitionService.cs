87fcf1f79188f3194bc97945d6bcc32c2cdd1b25
Neda Pirayesh <nd_pirayesh@yahoo.com>
2020-08-12 19:54:37 +0430

    add noationalinsurancecontributionservice

---
+++ PayCompute.Services/INationalInsuranceContribitionService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PayCompute.Services
+{
+    public interface INationalInsuranceContribitionService
+    {
+        decimal NIContribution(decimal totalAmount);
+    }
+}
