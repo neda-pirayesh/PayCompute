c962bb6f72c8ebc36358e5f8919e9031806c9635
Neda Pirayesh <nd_pirayesh@yahoo.com>
2020-08-12 20:22:16 +0430

    add payment record controller in paymentrecore index view model

---
+++ PayCompute.Services/ViewModels/PaymentRecordIndexViewModel.cs
@@ -0,0 +1,33 @@
+﻿using PayCompute.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Permissions;
+using System.Text;
+
+namespace PayCompute.Services.ViewModels
+{
+    public class PaymentRecordIndexViewModel
+    {
+        public int Id { get; set; }
+        public int EmployeeId { get; set; }
+        public Employee  Employee { get; set; }
+
+        [Display(Name="نام")]
+        public string FullName { get; set; }
+        [Display(Name = "تاریخ پرداخت")]
+        public DateTime PayDate { get; set; }
+        [Display(Name = "ماه پرداخت")]
+        public string PayMonth { get; set; }
+        public int TaxYearId { get; set; }
+        public string Year { get; set; }
+
+        [Display(Name = "درآمد کل")]
+        public decimal TotalEarnings { get; set; }
+        [Display(Name = "گسورات")]
+        public decimal Totaldeduction { get; set; }
+        [Display(Name = "Net")]
+        public decimal NetPayment { get; set; }
+
+    }
+}
---
+++ PayComputee/Controllers/PayController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PayCompute.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PayComputee.Controllers
+{
+    public class PayController:Controller
+    {
+        private readonly IPayComputationService _payComputationService;
+
+        public PayController(IPayComputationService payComputationService)
+        {
+            _payComputationService = payComputationService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
